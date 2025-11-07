e42b2f8f8ecc74a5e89dfb2bdf459c7e2cca7ed8
smagiero <smagiero@users.noreply.github.com>
2025-11-07 08:06:39 -0500

    update test case

--- max/brute/tb/asic.t.v
+++ max/brute/tb/asic.t.v
@@ -5,7 +5,7 @@
 
 `define ASIC_IMPL               asic
 `define ASIC_IMPL_STR           "asic"
-`define ASIC_TEST_CASES_FILE    "asic-test-cases-decoder.v"
+`define ASIC_TEST_CASES_FILE    "asic-test-cases-1x2.v"
 
 `include "asic.v"
 `include "asic-test-harness.v"
